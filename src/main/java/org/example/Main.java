7018577366467d84c2b5fe62e5bf96d173e2376d
rfaGonzlz <rafael.gonzalez@est.iudigital.edu.co>
2025-06-07 15:30:16 -0500

    feat: instanciar objeto Profesor en Main. feat: establecer conexión desde Main. feat: guardar objeto Profesor usando DAO. feat: cerrar conexión tras guardar datos. feat: llamar agregar e imprimirRegistros().

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Crear un objeto Profesor y agregarlo a la lista
+        Profesor profesor = new Profesor("Juan", "Pérez", "01/03/1999", "Masculino", 1.75, 70.0, "Matemáticas");
+
+        // Establecer la conexión a la base de datos.
+        ConnectionDB connectionDB = new ConnectionDB();
+        Connection connection = connectionDB.getConnection();
+
+        //crear un DAO y guardar el profesor en la base de datos
+        DAO dao = new DAO();
+        dao.guardar(profesor, connection);
+
+        // Aquí podrías cerrar la conexión si es necesario
+        connectionDB.closeConnection(connection);
+
+        // Agregar el profesor al array y mostrar los registros.
+        profesor.agregar(profesor);
+        profesor.imprimirRegistros();
     }
 }
